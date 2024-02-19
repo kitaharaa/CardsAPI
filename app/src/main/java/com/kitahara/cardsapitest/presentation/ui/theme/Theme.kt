c357a0c9062f56bd38f0626d23419cad38c592e3
new.amogus3000 <bvalentin1214@gmail.com>
2024-02-19 11:22:56 +0200

    changed navBar color

--- app/src/main/java/com/kitahara/cardsapitest/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/kitahara/cardsapitest/presentation/ui/theme/Theme.kt
@@ -57,7 +57,9 @@ fun CardsAPITestTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
+            window.statusBarColor = colorScheme.onBackground.toArgb()
+            window.navigationBarColor = colorScheme.onSurface.toArgb()
+
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
